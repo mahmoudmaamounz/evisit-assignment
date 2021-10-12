d55457e9608bb16a271e67fdc98ea2b6e62b3a3a
Mahmoud Maamoun <mahmoud.admaamoun@gmail.com>
2021-10-12 06:57:22 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 
+import { mergeSort } from './sort';
+
 
 // Initialize the array that will be sorted on ascending order so index 0 is the top 100, index 1 is top 99 and so on. 
 const topHundred = new Array(100);
@@ -9,27 +11,15 @@ let ipAddressesMap = {};
  * @param ip_address ip address 
  * @returns void
 */
+// Time complexity nLogn 
 const request_handled = (ip_address): void => {
 
 if(!ip_address){
 throw new Error('BAD_IP_ADDRESS');
 }
-
-
-topHundred.forEach((ip, index)=>{
-
-if(ip !== ip_address && ipAddressesMap[ip] < ipAddressesMap[ip_address]){
-
-    // If its already in the top one hundred array then remove it
-    if(topHundred.indexOf(ip_address) !== -1){
-        topHundred.splice(index, 1);
-    }
-
-    // Add the ip to the top one hundred
-    topHundred.splice(index, 0, ip_address);
-}
-});
-
+    
+// Time complexity nLogn 
+mergeSort(topHundred, ipAddressesMap);
 }
 
 /**
